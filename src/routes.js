80587e73f2414e624faf281edd284b5571743686
kamal14rajput <kamal14rajput@gmail.com>
2022-04-13 03:16:31 +0530

    7th

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const controller = require('../controller/controller')
+const controller = require('/controller')
 
 module.exports = (routes) => {
     routes.get('/hello_world', controller.messageController);
